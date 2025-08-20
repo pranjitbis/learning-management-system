77305fe674a9db16ee215baa1095dda7c49f1ff4
pranjitbis <Pranjitbiswas77@gmail.com>
2025-08-20 17:50:50 +0530

    first

--- app/components/Footer/Page.jsx
+++ app/components/Footer/Page.jsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 import Image from 'next/image';
 import styles from './Footer.module.css';
 
-export default function Footer() {
+export default function Page() {
   return (
     <footer className={styles.footer}>
       <div className={styles.footerContainer}>
